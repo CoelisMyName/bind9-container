f836e14dbdb91ef9446c84c340a1ca68bacdb68e
coel <coel@example.com>
2022-11-22 00:37:16 +0800

    feat: 完成dns配置

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
+rm -rf ~/bind9
 mkdir -p ~/bind9/etc/bind
-sudo cp bind9/etc/bind/named.conf.options ~/bind9/etc/bind
-sudo cp bind9/etc/bind/named.conf.default-zones ~/bind9/etc/bind
-sudo cp bind9/etc/bind/db.subnet ~/bind9/etc/bind
+mkdir -p ~/bind9/var/cache/bind
+mkdir -p ~/bind9/var/lib/bind
 
-docker run -d --name bind9-container \
--e TZ=UTC -p 53:53 ubuntu/bind9:latest \
--v ~/bind9/etc/bind/named.conf.options:/etc/bind/named.conf.options
+cp bind9/etc/bind/named.conf.options ~/bind9/etc/bind
+cp bind9/etc/bind/named.conf.default-zones ~/bind9/etc/bind
+cp bind9/var/lib/bind/db.* ~/bind9/var/lib/bind
+
+sudo chmod 777 ~/bind9/var/cache/bind
+
+docker run -d \
+--name bind9-container \
+-e TZ=UTC \
+-p 30053:53/tcp \
+-p 30053:53/udp \
+-p 30953:953/tcp \
+-e BIND9_USER=bind \
+-v ~/bind9/etc/bind/named.conf.options:/etc/bind/named.conf.options \
+-v ~/bind9/etc/bind/named.conf.default-zones:/etc/bind/named.conf.default-zones \
+-v ~/bind9/etc/bind/db.cloud-service:/etc/bind/db.cloud-service \
+-v ~/bind9/var/cache/bind:/var/cache/bind \
+-v ~/bind9/var/lib/bind:/var/lib/bind \
+ubuntu/bind9:latest
+
+nslookup -port=30053 hcr.cloud-service 127.0.0.1
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 sudo cp bind9/etc/bind/named.conf.options /etc/bind
 sudo cp bind9/etc/bind/named.conf.default-zones /etc/bind
-sudo cp bind9/etc/bind/db.subnet /etc/bind
+sudo cp bind9/var/lib/bind/db.* /var/lib/bind
+sudo named-checkconf
